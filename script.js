d2acc0e20964ac3a99e8cf5f5aca7ff65e9bc565
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-01-30 15:45:43 +0100

    prove

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ async function getPosts() {
     
     // .media_details.sizes.large
     for (i = 0 ; i < result.length; i++) {
-
-        if (i > 3) {
-            break;
-        }
+        // const firstSlide = [0][1][2][3] ;
+        const secondSlide = "";
+        const thirdSlide = "";
 
         const image = result[i]._embedded["wp:featuredmedia"][0].source_url;
         const title = result[i].title.rendered;
         const previousText = result[i].excerpt.rendered;
 
         postContainer.innerHTML += 
-        //EXPERIMENT
-        // `<div class="content">
-        // ${result[i].content.rendered}
-        // </div>`
+        // `<div class="card-post">
+        // <h2>${result[firstSlide].title.rendered}</h2>
+        // <img src="${result[firstSlide]._embedded["wp:featuredmedia"][0].source_url}" alt="image of something" />
+        // <p>${result[firstSlide].excerpt.rendered}</p>
+        // </div>`;
         `<div class="card-post">
         <h2>${title}</h2>
         <img src="${image}" alt="image of something" />
