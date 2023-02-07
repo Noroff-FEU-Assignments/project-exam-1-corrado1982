660fcf88d8467e4040f19cc9469dd963bf637de1
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-02-07 14:15:27 +0100

    wip

---
+++ scripts/contact.js
@@ -0,0 +1,11 @@
+// console.log("wei ciao")
+
+const nameField = document.querySelector("#full-name");
+const emailField = document.querySelector("#e-mail");
+const subjectField = document.querySelector("#subject");
+const messageField = document.querySelector("#message");
+
+// nameField.addEventListener {
+
+// }
+
