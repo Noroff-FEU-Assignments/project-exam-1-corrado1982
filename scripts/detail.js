caeb4ee7babe9865836b2c030f005b4a4892ef41
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-02-08 15:15:18 +0100

    fixing modal

--- scripts/detail.js
+++ scripts/detail.js
@@ -2,6 +2,7 @@ const detailContainer = document.querySelector(".detail-container");
 const titleContainer = document.querySelector(".title");
 const imageContainer = document.querySelector(".image-preview");
 const textContainer = document.querySelector(".text-container");
+const modalContainer = document.querySelector(".modal-container");
 const bigPic = document.querySelector(".big-pic");
 
 const queryString = document.location.search;
@@ -21,11 +22,12 @@ async function getDetailPost() {
         // const previousText = result.excerpt.rendered;
         //const image = result._embedded["wp:featuredmedia"][0].source_url;
         const image = result._embedded["wp:featuredmedia"][0].source_url;
-
+        const modalImage = result._embedded["wp:featuredmedia"][0].source_url;
         
         titleContainer.innerHTML = `<h2>${title}</h2>`;
         imageContainer.innerHTML = `<img class="pic-to-click" src="${image}"/>`;
         textContainer.innerHTML = `<div>${result.content.rendered} </div>`;
+        
         // detailContainer.innerHTML = `<div> <img class="big-pic" src="${result._embedded["wp:featuredmedia"][0].source_url}" alt="we"/> </div>`;
 
     // detailContainer.innerHTML = 
@@ -37,12 +39,18 @@ async function getDetailPost() {
     // </div>
     // <div> <img class="big-pic" src="${result._embedded["wp:featuredmedia"][0].source_url}" alt="we"/> </div>
     // `;
+    imageContainer.onclick = function (){
+        modalContainer.innerHTML = `<img class="modal-content" src="${result._embedded["wp:featuredmedia"][0].source_url}"/>`;
+        console.log("clicked")
+    // bigPic.style.display = "block";
+    }
 };
 getDetailPost(url);
 //<div> <img src="${result._embedded["wp:featuredmedia"][0].source_url}" alt="we"/> </div>
 // const picToClick = document.querySelector(".pic-to-click");
 // console.log(picToClick);
-imageContainer.onclick = function (){
-    console.log("clicked")
+// imageContainer.onclick = function (){
+//     modalContainer.innerHTML = `<img class="modal-content" src="${result._embedded["wp:featuredmedia"][0].source_url}"/>`;
+//     console.log("clicked")
 // bigPic.style.display = "block";
-}
+// }
