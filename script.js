b960a0b9577b4cdd05d6357a77360c095ca2334e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-02-14 16:13:13 +0100

    wip

--- script.js
+++ script.js
@@ -39,31 +39,31 @@ async function getPosts(url) {
         </div>`;
         
         
-        let slideIndex = 1 ;
-        showSlides(slideIndex);
+        // let slideIndex = 1 ;
+        // showSlides(slideIndex);
         
-        function plusSlides(n) {
-          showSlides(slideIndex += n);
-        }
-           function showSlides(n) {
-            let slides = document.getElementsByClassName("card-row");
-            // console.log(slides);
-            if (n > slides.length) {slideIndex = 1};
-            if (n < 1) {slideIndex = slides.length};
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-              }
-              slides[slideIndex-1].style.display = "block";
-            //   slides[slideIndex-1].style.display += "block";
-            //   slides[slideIndex].style.display = "block";
+        // function plusSlides(n) {
+        //   showSlides(slideIndex += n);
+        // }
+        //    function showSlides(n) {
+        //     let slides = document.getElementsByClassName("card-row");
+        //     // console.log(slides);
+        //     if (n > slides.length) {slideIndex = 1};
+        //     if (n < 1) {slideIndex = slides.length};
+        //     for (i = 0; i < slides.length; i++) {
+        //         slides[i].style.display = "none";  
+        //       }
+        //       slides[slideIndex-1].style.display = "block";
+        //     //   slides[slideIndex-1].style.display += "block";
+        //     //   slides[slideIndex].style.display = "block";
               
-            }
-            preArrow.onclick = function(){
-                plusSlides(-1);
-            };
-            newArrow.onclick = function(){
-                plusSlides(1);
-                };
+        //     }
+        //     preArrow.onclick = function(){
+        //         plusSlides(-1);
+        //     };
+        //     newArrow.onclick = function(){
+        //         plusSlides(1);
+        //         };
     };
 
 
