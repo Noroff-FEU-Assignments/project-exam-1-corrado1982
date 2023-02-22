c9d0a8cfce77aadb80448a7ce295c5855ba43ed7
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-02-22 14:43:08 +0100

    wip

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,10 +1,10 @@
 const postsContainer = document.querySelector(".posts-container");
 const nextPosts = document.querySelector(".next-posts");
-// var page = 2;
-var page = 1;
-const pageUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page" + page;
+var page = 2;
+// var page = 1;
+// const pageUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page" + page;
 
-// const pageUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page=1";
+const pageUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page=1";
 const baseUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page=";
 
 
@@ -13,7 +13,7 @@ async function getCards (url) {
     const data = await fetch(url);
     const result = await data.json();
     console.log(result);
-    // postsContainer.innerHTML = "";
+    postsContainer.innerHTML = "";
     for (i = 0 ; i < result.length; i++) {
         const id = result[i].id;
         const image = result[i]._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
@@ -40,10 +40,40 @@ async function getCards (url) {
 getCards(pageUrl);
 
 nextPosts.onclick = function() {
-    const addPage = page + 1;
+    const addPage = page ++ ;
     const newUrl = baseUrl + addPage;
-    // const newUrl = baseUrl + addPage;
     
-   getCards(newUrl);
+   newCards(newUrl);
 }
 
+//prova
+
+
+async function newCards (url) {
+    const data = await fetch(url);
+    const result = await data.json();
+    console.log(result);
+    
+    for (i = 0 ; i < result.length; i++) {
+        const id = result[i].id;
+        const image = result[i]._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+        const title = result[i].title.rendered;
+        const previousText = result[i].excerpt.rendered;
+
+        if(result.length < 10) {
+            nextPosts.style.display = "none";
+            console.log("don t display it");
+        }
+        // detail.html?id=${id}
+        
+        postsContainer.innerHTML += 
+        `<div class="card-post">
+        <a href="detail.html?id=${id}">
+        <h2>${title}</h2>
+        <img src="${image}" alt="image of ${title}" />
+        <p>${previousText}</p>
+        </a>
+        </div>`
+        ;
+    };
+}
