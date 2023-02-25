a2577c333882c3da70830c59995bf88b9bc43035
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-02-25 16:06:59 +0100

    trying with search filter...

--- scripts/posts.js
+++ scripts/posts.js
@@ -9,14 +9,17 @@ const pageUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?p
 const baseUrl = "https://issimo.one/ancient-civilizations/wp-json/wp/v2/posts/?per_page=10&_embed&page=";
 
 
-// search.onkeyup = function() {
-//     console.log(event.target.value);
-// }
+search.onkeyup = function() {
+    console.log(event.target.value);
+}
 
 async function getCards (url) {
     const data = await fetch(url);
     const result = await data.json();
     console.log(result);
+
+    
+
     postsContainer.innerHTML = "";
     for (i = 0 ; i < result.length; i++) {
         const id = result[i].id;
@@ -24,26 +27,25 @@ async function getCards (url) {
         const title = result[i].title.rendered;
         const previousText = result[i].excerpt.rendered;
 
+        
         console.log(result[i].title.rendered);
-
+        
         // search.onkeyup = function() {
         //     console.log(event.target.value);
-            
         // }
-        // const filteredPots = title.filter(filterPosts);
+
+        // const filteredPots = result.filter(filterPosts);
         // function filterPosts(posts) {
-        //     if(posts.toLowerCase().startsWith(event.target.value)){
-        //         return true;
+        //     if(posts.title.rendered.includes(event.target.value)){
+        //         console.log("ma allora funziona?");
         //     }
         // }
-        
+        // console.log(filteredPots);
 
         if(result.length < 10) {
             nextPosts.style.display = "none";
-            console.log("don t display it");
-        }
-        // detail.html?id=${id}
-        
+        }  
+
         postsContainer.innerHTML += 
         `<div class="card-post">
         <a href="detail.html?id=${id}">
@@ -53,8 +55,10 @@ async function getCards (url) {
         </a>
         </div>`
         ;
+       
     };
 }
+ 
 getCards(pageUrl);
 
 nextPosts.onclick = function() {
@@ -81,7 +85,6 @@ async function newCards (url) {
         if(result.length < 10) {
             nextPosts.style.display = "none";
         }
-        // detail.html?id=${id}
         
         postsContainer.innerHTML += 
         `<div class="card-post">
